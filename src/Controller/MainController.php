1f1ffde6e7c389f159187273511c295e42e4e17d
Franck-dev-hub <franck.spadotto@yahoo.com>
2026-05-16 14:26:01 +0200

    feat(homepage): add twig & debug packages

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-luc Pickles',
+            'status' => 'Under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig',
+            [
+                'starshipCount' => $starshipCount,
+                'myShip' => $myShip,
+            ]);
     }
 }
